27c355e6fb3d90cb0fae29c854ec6bb37eb63369
Anton <raevsanton@gmail.com>
2025-01-19 23:41:02 +0100

    Add user service and payload; implement liked tracks retrieval in playlist service

--- internal/playlist/payload.go
+++ internal/playlist/payload.go
@@ -1,14 +1,31 @@
 package playlist
 
 type PlaylistRequest struct {
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	Public       bool   `json:"is_public"`
-	AccessToken  string `json:"access_token" validate:"required"`
-	RefreshToken string `json:"refresh_token" validate:"required"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Public      bool   `json:"is_public"`
+	AccessToken string `json:"access_token" validate:"required"`
 }
 
 type PlaylistResponse struct {
 	PlaylistId  string `json:"playlist_id"`
 	AccessToken string `json:"access_token"`
+}
+
+type LikedTracksResponse struct {
+	Href   string `json:"href"`
+	Limit  int    `json:"limit"`
+	Offset int    `json:"offset"`
+	Total  int    `json:"total"`
+	Items  []Item `json:"items"`
+}
+
+type Item struct {
+	AddedAt string `json:"added_at"`
+	Track   Track  `json:"track"`
+}
+
+type Track struct {
+	Uri string `json:"uri"`
+	// more fields
 }
--- internal/playlist/service.go
+++ internal/playlist/service.go
@@ -1,7 +1,11 @@
 package playlist
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/raevsanton/sharify-backend/configs"
+	"github.com/raevsanton/sharify-backend/pkg/codec"
 )
 
 type PlaylistService struct {
@@ -11,6 +15,60 @@ func NewPlaylistService() *PlaylistService {
 	return &PlaylistService{}
 }
 
-func (service *PlaylistService) GeneratePlaylist(body PlaylistRequest, config *configs.Config) (PlaylistResponse, error) {
+func (service *PlaylistService) CreatePlaylist(body PlaylistRequest) (URIs []string, total int, err error) {
 
+}
+
+func (service *PlaylistService) GetURIsLikedTracks(body PlaylistRequest, offset int) (URIs []string, total int, err error) {
+	url := fmt.Sprintf("https://api.spotify.com/v1/me/tracks?offset=%d&limit=50", offset)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+body.AccessToken)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, 0, fmt.Errorf("failed to fetch liked tracks: %w", err)
+	}
+
+	tracks, err := codec.Decode[LikedTracksResponse](res.Body)
+	if err != nil {
+		return nil, 0, fmt.Errorf("failed to get liked tracks: %w", err)
+	}
+
+	var uris []string
+	for _, item := range tracks.Items {
+		uris = append(uris, item.Track.Uri)
+	}
+
+	return uris, tracks.Total, nil
+
+}
+
+func (service *PlaylistService) GeneratePlaylist(body PlaylistRequest, config *configs.Config) (PlaylistResponse, error) {
+	var accumulatedURIs []string
+	totalURIs := 0
+	offset := 0
+
+	for {
+		URIs, total, err := service.GetURIsLikedTracks(body, offset)
+		if err != nil {
+			return PlaylistResponse{}, fmt.Errorf("failed to get liked tracks URIs: %w", err)
+		}
+		if totalURIs == 0 {
+			totalURIs = total
+		}
+		offset += 50
+		accumulatedURIs = append(accumulatedURIs, URIs...)
+
+		if totalURIs > len(accumulatedURIs) {
+			break
+		}
+	}
+
+	return PlaylistResponse{}, fmt.Errorf("failed to get liked tracks: %w", nil)
 }
